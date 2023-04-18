1f121bded2dc9f4a231c59cbc1c5618b2aa5af80
Gary Feng <gary.feng@gmail.com>
2023-04-18 12:13:53 -0400

    very hacky fix of #6, reutrning empty string or none

--- scripts/llm_utils.py
+++ scripts/llm_utils.py
@@ -1,5 +1,7 @@
 import openai
+from logger import logger
 from config import Config
+
 cfg = Config()
 
 openai.api_key = cfg.openai_api_key
@@ -7,20 +9,39 @@ openai.api_key = cfg.openai_api_key
 # Overly simple abstraction until we create something better
 def create_chat_completion(messages, model=None, temperature=None, max_tokens=None)->str:
     """Create a chat completion using the OpenAI API"""
-    if cfg.use_azure:
-        response = openai.ChatCompletion.create(
-            deployment_id=cfg.azure_chat_deployment_id,
-            model=model,
-            messages=messages,
-            temperature=temperature,
-            max_tokens=max_tokens
-        )
-    else:
-        response = openai.ChatCompletion.create(
-            model=model,
-            messages=messages,
-            temperature=temperature,
-            max_tokens=max_tokens
-        )
+    # very crude handling of OpenAPI errors: we simply return an empty string 
+    logger.debug(f"Tokens remaining for response: {tokens_remaining}")
+    logger.debug("------------ CONTEXT SENT TO AI ---------------")
+    try:
+        if cfg.use_azure:
+            response = openai.ChatCompletion.create(
+                deployment_id=cfg.azure_chat_deployment_id,
+                model=model,
+                messages=messages,
+                temperature=temperature,
+                max_tokens=max_tokens
+            )
+        else:
+            response = openai.ChatCompletion.create(
+                model=model,
+                messages=messages,
+                temperature=temperature,
+                max_tokens=max_tokens
+            )
 
-    return response.choices[0].message["content"]
+        return response.choices[0].message["content"]
+    except openai.error.APIError as e:
+        #Handle API error here, e.g. retry or log
+        logger.debug(f"OpenAI API returned an API Error: {e}")
+        pass
+    except openai.error.APIConnectionError as e:
+        #Handle connection error here
+        logger.debug(f"Failed to connect to OpenAI API: {e}")
+        pass
+    except openai.error.RateLimitError as e:
+        #Handle rate limit error (we recommend using exponential backoff)
+        logger.debug(f"OpenAI API request exceeded rate limit: {e}")
+        pass
+    # in case of the above errors, returns an empty string
+    return ""
+    
--- scripts/memory/base.py
+++ scripts/memory/base.py
@@ -2,15 +2,34 @@
 import abc
 from config import AbstractSingleton, Config
 import openai
+from logger import logger
+
 cfg = Config()
 
 
 def get_ada_embedding(text):
     text = text.replace("\n", " ")
-    if cfg.use_azure:
-        return openai.Embedding.create(input=[text], engine=cfg.azure_embeddigs_deployment_id, model="text-embedding-ada-002")["data"][0]["embedding"]
-    else:
-        return openai.Embedding.create(input=[text], model="text-embedding-ada-002")["data"][0]["embedding"]
+    # very crude handling of OpenAPI errors: we simply return none
+    try:
+        if cfg.use_azure:
+            return openai.Embedding.create(input=[text], engine=cfg.azure_embeddigs_deployment_id, model="text-embedding-ada-002")["data"][0]["embedding"]
+        else:
+            return openai.Embedding.create(input=[text], model="text-embedding-ada-002")["data"][0]["embedding"]
+    except openai.error.APIError as e:
+        #Handle API error here, e.g. retry or log
+        logger.debug(f"OpenAI API returned an API Error: {e}")
+        pass
+    except openai.error.APIConnectionError as e:
+        #Handle connection error here
+        logger.debug(f"Failed to connect to OpenAI API: {e}")
+        pass
+    except openai.error.RateLimitError as e:
+        #Handle rate limit error (we recommend using exponential backoff)
+        logger.debug(f"OpenAI API request exceeded rate limit: {e}")
+        pass
+    # in case of the above errors, returns none
+    return None
+    
 
 
 class MemoryProviderSingleton(AbstractSingleton):
